bd6766905aac59ffe69d6625e3c12d229c9f7910
RichH <richhowland@netzero.net>
2024-07-31 06:58:49 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
         <h2>(test-web-app05a)</h2>
       </div>
       <div>
-        <h3>minor revision to confirm Git Pull during deployment run</h3>
+        <h3>minor revision to confirm Git Pull during deployment run<br />
+        second revision.        
+        </h3>
       </div>
     </div>
   );
